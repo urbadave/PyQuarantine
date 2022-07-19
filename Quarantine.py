5a5723e73393c0013bdfa4d8c2c9ce9c3a5746a4
Urbanek, David <david.urbanek@aruplab.com>
2022-07-19 07:54:09 -0600

    Just fiddling

--- Quarantine.py
+++ Quarantine.py
@@ -3,6 +3,7 @@ from datetime import date
 from Workdays import computeDays
 
 begin = date(2020, 3, 16)
+#begin = date(2020, 9, 9)
 end = date.today()
 result = computeDays(begin, end)
 
@@ -10,4 +11,4 @@ print('Total of', result.allDays, 'days since', result.startDay)
 print(result.workDays, 'were work days')
 print(result.weekendDays, 'were weekend days')
 print(result.holidayDays, 'were holidays')
-input('Press ENTER to exit')
+input('Press ENTER to exit') 
